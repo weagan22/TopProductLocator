3ffc25b0d3c09ab80bfb551cb926dfff59545765
weagan22 <weagan22@gmail.com>
2024-04-12 17:25:29 -0700

    Added comments, simplified, and updated user interface.

--- TopProductLocator/MainForm.Designer.vb
+++ TopProductLocator/MainForm.Designer.vb
@@ -23,77 +23,93 @@ Partial Class MainForm
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Box_FilePath = New System.Windows.Forms.GroupBox()
         Me.Txt_FilePath = New System.Windows.Forms.TextBox()
-        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.LayoutPanel_PathButton = New System.Windows.Forms.TableLayoutPanel()
         Me.Btn_GetTopProd = New System.Windows.Forms.Button()
-        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.LayoutPanel_Global = New System.Windows.Forms.TableLayoutPanel()
+        Me.Box_TopProds = New System.Windows.Forms.GroupBox()
         Me.List_OutValues = New System.Windows.Forms.ListBox()
-        Me.GroupBox1.SuspendLayout()
-        Me.TableLayoutPanel2.SuspendLayout()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Box_FilePath.SuspendLayout()
+        Me.LayoutPanel_PathButton.SuspendLayout()
+        Me.LayoutPanel_Global.SuspendLayout()
+        Me.Box_TopProds.SuspendLayout()
         Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'GroupBox1
+        'Box_FilePath
         '
-        Me.GroupBox1.Controls.Add(Me.Txt_FilePath)
-        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.GroupBox1.Location = New System.Drawing.Point(3, 3)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(394, 58)
-        Me.GroupBox1.TabIndex = 0
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "File Path"
+        Me.Box_FilePath.Controls.Add(Me.Txt_FilePath)
+        Me.Box_FilePath.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Box_FilePath.Location = New System.Drawing.Point(3, 3)
+        Me.Box_FilePath.Name = "Box_FilePath"
+        Me.Box_FilePath.Size = New System.Drawing.Size(478, 44)
+        Me.Box_FilePath.TabIndex = 0
+        Me.Box_FilePath.TabStop = False
+        Me.Box_FilePath.Text = "File Path"
         '
         'Txt_FilePath
         '
         Me.Txt_FilePath.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Txt_FilePath.Location = New System.Drawing.Point(3, 16)
         Me.Txt_FilePath.Name = "Txt_FilePath"
-        Me.Txt_FilePath.Size = New System.Drawing.Size(388, 20)
+        Me.Txt_FilePath.Size = New System.Drawing.Size(472, 20)
         Me.Txt_FilePath.TabIndex = 0
         Me.Txt_FilePath.Text = "File path here..."
         '
-        'TableLayoutPanel2
+        'LayoutPanel_PathButton
         '
-        Me.TableLayoutPanel2.ColumnCount = 2
-        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 176.0!))
-        Me.TableLayoutPanel2.Controls.Add(Me.Btn_GetTopProd, 1, 0)
-        Me.TableLayoutPanel2.Controls.Add(Me.GroupBox1, 0, 0)
-        Me.TableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TableLayoutPanel2.Location = New System.Drawing.Point(3, 3)
-        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
-        Me.TableLayoutPanel2.RowCount = 1
-        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel2.Size = New System.Drawing.Size(576, 64)
-        Me.TableLayoutPanel2.TabIndex = 0
+        Me.LayoutPanel_PathButton.ColumnCount = 2
+        Me.LayoutPanel_PathButton.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.LayoutPanel_PathButton.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150.0!))
+        Me.LayoutPanel_PathButton.Controls.Add(Me.Box_TopProds, 0, 0)
+        Me.LayoutPanel_PathButton.Controls.Add(Me.TableLayoutPanel1, 1, 0)
+        Me.LayoutPanel_PathButton.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.LayoutPanel_PathButton.Location = New System.Drawing.Point(3, 53)
+        Me.LayoutPanel_PathButton.Name = "LayoutPanel_PathButton"
+        Me.LayoutPanel_PathButton.RowCount = 1
+        Me.LayoutPanel_PathButton.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.LayoutPanel_PathButton.Size = New System.Drawing.Size(478, 130)
+        Me.LayoutPanel_PathButton.TabIndex = 0
         '
         'Btn_GetTopProd
         '
         Me.Btn_GetTopProd.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Btn_GetTopProd.Enabled = False
-        Me.Btn_GetTopProd.Location = New System.Drawing.Point(403, 3)
+        Me.Btn_GetTopProd.Location = New System.Drawing.Point(3, 3)
         Me.Btn_GetTopProd.Name = "Btn_GetTopProd"
-        Me.Btn_GetTopProd.Size = New System.Drawing.Size(170, 58)
+        Me.Btn_GetTopProd.Size = New System.Drawing.Size(138, 44)
         Me.Btn_GetTopProd.TabIndex = 1
-        Me.Btn_GetTopProd.Text = "Locate Top Product"
+        Me.Btn_GetTopProd.Text = "Locate"
         Me.Btn_GetTopProd.UseVisualStyleBackColor = True
         '
-        'TableLayoutPanel1
+        'LayoutPanel_Global
         '
-        Me.TableLayoutPanel1.ColumnCount = 1
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.TableLayoutPanel1.Controls.Add(Me.List_OutValues, 0, 1)
-        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 0, 0)
-        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
-        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
-        Me.TableLayoutPanel1.RowCount = 2
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 70.0!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(582, 275)
-        Me.TableLayoutPanel1.TabIndex = 1
+        Me.LayoutPanel_Global.ColumnCount = 1
+        Me.LayoutPanel_Global.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.LayoutPanel_Global.Controls.Add(Me.LayoutPanel_PathButton, 0, 1)
+        Me.LayoutPanel_Global.Controls.Add(Me.Box_FilePath, 0, 0)
+        Me.LayoutPanel_Global.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.LayoutPanel_Global.Location = New System.Drawing.Point(0, 0)
+        Me.LayoutPanel_Global.Name = "LayoutPanel_Global"
+        Me.LayoutPanel_Global.RowCount = 2
+        Me.LayoutPanel_Global.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50.0!))
+        Me.LayoutPanel_Global.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.LayoutPanel_Global.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.LayoutPanel_Global.Size = New System.Drawing.Size(484, 186)
+        Me.LayoutPanel_Global.TabIndex = 1
+        '
+        'Box_TopProds
+        '
+        Me.Box_TopProds.Controls.Add(Me.List_OutValues)
+        Me.Box_TopProds.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Box_TopProds.Location = New System.Drawing.Point(3, 3)
+        Me.Box_TopProds.Name = "Box_TopProds"
+        Me.Box_TopProds.Size = New System.Drawing.Size(322, 124)
+        Me.Box_TopProds.TabIndex = 2
+        Me.Box_TopProds.TabStop = False
+        Me.Box_TopProds.Text = "Located Top Product(s)"
         '
         'List_OutValues
         '
@@ -101,32 +117,51 @@ Partial Class MainForm
         Me.List_OutValues.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.List_OutValues.FormattingEnabled = True
         Me.List_OutValues.ItemHeight = 16
-        Me.List_OutValues.Location = New System.Drawing.Point(3, 73)
+        Me.List_OutValues.Location = New System.Drawing.Point(3, 16)
         Me.List_OutValues.Name = "List_OutValues"
-        Me.List_OutValues.Size = New System.Drawing.Size(576, 199)
+        Me.List_OutValues.Size = New System.Drawing.Size(316, 105)
         Me.List_OutValues.TabIndex = 1
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Btn_GetTopProd, 0, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(331, 3)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(144, 124)
+        Me.TableLayoutPanel1.TabIndex = 3
         '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(582, 275)
-        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.ClientSize = New System.Drawing.Size(484, 186)
+        Me.Controls.Add(Me.LayoutPanel_Global)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MinimumSize = New System.Drawing.Size(400, 175)
         Me.Name = "MainForm"
         Me.Text = "Top Product Locator"
-        Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
-        Me.TableLayoutPanel2.ResumeLayout(False)
+        Me.Box_FilePath.ResumeLayout(False)
+        Me.Box_FilePath.PerformLayout()
+        Me.LayoutPanel_PathButton.ResumeLayout(False)
+        Me.LayoutPanel_Global.ResumeLayout(False)
+        Me.Box_TopProds.ResumeLayout(False)
         Me.TableLayoutPanel1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+    Friend WithEvents Box_FilePath As GroupBox
+    Friend WithEvents LayoutPanel_PathButton As TableLayoutPanel
     Friend WithEvents Txt_FilePath As TextBox
     Friend WithEvents Btn_GetTopProd As Button
-    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents LayoutPanel_Global As TableLayoutPanel
     Friend WithEvents List_OutValues As ListBox
+    Friend WithEvents Box_TopProds As GroupBox
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
 End Class
--- TopProductLocator/MainForm.vb
+++ TopProductLocator/MainForm.vb
@@ -5,55 +5,6 @@ Public Class MainForm
 
     Public Shared Property CATIA As Application = Nothing
 
-    Private Sub Btn_GetTopProd_Click(sender As Object, e As EventArgs) Handles Btn_GetTopProd.Click
-        Try
-            'Clear the return box
-            List_OutValues.Items.Clear()
-
-            'Make sure CATIA is open and available
-            If GetCATIA() Then
-                Dim FilePath As String = Txt_FilePath.Text
-
-                Dim FileList() As String = System.IO.Directory.GetFiles(FilePath)
-                Dim ProductList = New List(Of ProductFile)
-
-                For i As Integer = 0 To UBound(FileList)
-                    Dim StrFile As String = FileList(i)
-
-                    If System.IO.Path.GetExtension(StrFile) = ".CATProduct" Then
-                        ProductList.Add(New ProductFile(StrFile))
-                    End If
-                Next
-
-
-                For Each Prod In ProductList
-                    For Each PotentialParent In ProductList
-                        If PotentialParent.CheckIsChild(Prod.FullName) Then
-                            Prod.IsChild = True
-                            Exit For
-                        End If
-                    Next
-                Next
-
-
-                For Each Prod In ProductList
-                    If Not Prod.IsChild Then
-                        List_OutValues.Items.Add(Prod.Name)
-                    End If
-                Next
-
-                If List_OutValues.Items.Count = 0 Then
-                    List_OutValues.Items.Add("Error: No CATProducts found in the directory")
-                End If
-
-
-            End If
-
-        Catch ex As Exception
-            MsgBox("Error occured while locating top product." & vbNewLine & vbNewLine & ex.Message, MessageBoxIcon.Warning, "Error")
-        End Try
-    End Sub
-
     Private Sub Txt_FilePath_TextChanged(sender As Object, e As EventArgs) Handles Txt_FilePath.TextChanged
         If System.IO.Directory.Exists(Txt_FilePath.Text) Then
             Btn_GetTopProd.Enabled = True
@@ -61,6 +12,89 @@ Public Class MainForm
         Else
             Btn_GetTopProd.Enabled = False
             Txt_FilePath.BackColor = Color.Salmon
+        End If
+    End Sub
+
+    Private Sub Btn_GetTopProd_Click(sender As Object, e As EventArgs) Handles Btn_GetTopProd.Click
+        Try
+            'Clear the return box
+            List_OutValues.Items.Clear()
+
+
+            If GetCATIA() Then 'Make sure CATIA is open and available
+                'Get the file path from the form
+                Dim FilePath As String = Txt_FilePath.Text
+
+                'Load the list of products (get their children)
+                Dim ProductList = GetProducts(FilePath)
+
+                'Check each product to see if it is a child of any of the others
+                Call CheckChildren(ProductList)
+
+                'Output back to the form
+                OutputTopProducts(ProductList)
+
+            End If
+
+        Catch ex As Exception 'Handle catastrophic error
+            MsgBox("Error occured while locating top product." & vbNewLine & vbNewLine & ex.Message, MessageBoxIcon.Warning, "Error")
+
+            Try 'Finally, make sure file alerts get turned back on in CATIA
+                MainForm.CATIA.DisplayFileAlerts = True
+            Catch ex2 As Exception
+
+            End Try
+        End Try
+    End Sub
+
+    Function GetProducts(FilePath As String) As List(Of ProductFile)
+        Dim FileList() As String = System.IO.Directory.GetFiles(FilePath)
+        Dim RetProductList = New List(Of ProductFile)
+
+        'Get all of the products and their children in the directory
+        For i As Integer = 0 To UBound(FileList)
+            Dim StrFile As String = FileList(i)
+
+            Dim ExtType As String = System.IO.Path.GetExtension(StrFile)
+            If ExtType = ".CATProduct" Or ExtType = ".CATPart" Then
+                'This conditional limits the search to only .CATProduct files.
+                'If you would also like to return other file types
+                'in the directory that don't have a parent then you
+                'can remove this conditional. You could also extend it to other
+                'specific types like '... OR ExtType = ".CATPart" Then'
+                'for example.
+
+                RetProductList.Add(New ProductFile(StrFile))
+            End If
+        Next
+
+        Return RetProductList
+    End Function
+
+    Sub CheckChildren(ProductList As List(Of ProductFile))
+        'Check each of the products to see if they are a child of any of the others
+
+        For Each Prod In ProductList 'Check each product
+            For Each PotentialParent In ProductList 'Against each of the other products
+                If PotentialParent.IsChild(Prod.FullName) Then 'If is child then set to true for that product
+                    Prod.Child = True
+                    Exit For
+                End If
+            Next
+        Next
+    End Sub
+
+    Sub OutputTopProducts(ProductList As List(Of ProductFile))
+        'For each of the products, output to the listbox if the IsChild handle has not been set
+        For Each Prod In ProductList
+            If Not Prod.Child Then
+                List_OutValues.Items.Add(Prod.Name)
+            End If
+        Next
+
+        'Handle no products in the directory
+        If List_OutValues.Items.Count = 0 Then
+            List_OutValues.Items.Add("Error: No CATProducts found in the directory")
         End If
     End Sub
 
@@ -96,26 +130,32 @@ Public Class MainForm
     End Function
 End Class
 
-Class ProductFile
-    Public FullName As String
-    Public Name As String
-    Public Children = New List(Of String)
-    Public IsChild As Boolean = False
+Public Class ProductFile
+    Public FullName As String 'Full file path for the product
+    Public Name As String 'The name of the file
+    Public Children = New List(Of String) 'List of all child products
+    Public Child As Boolean = False 'Boolean for if this product is a child of others
 
     Sub New(FilePath As String)
         FullName = FilePath
         Name = System.IO.Path.GetFileName(FilePath)
 
+        'Open the file for reading in CATIA
+        MainForm.CATIA.DisplayFileAlerts = False
         Dim Doc As Document = MainForm.CATIA.Application.Documents.Read(FilePath)
+        MainForm.CATIA.DisplayFileAlerts = True
 
+        'Find all of its child products
         GetChildren(Doc.Product)
 
+        'Close the document for reading
         Doc.Close()
     End Sub
 
-    Function CheckIsChild(InFullName As String) As Boolean
-        For Each Child In Children
-            If Child = InFullName Then
+    Function IsChild(InFullName As String) As Boolean
+        'If the InFullName is contained in the children then it is a child, return true. Otherwise false.
+        For Each MyChild In Children
+            If MyChild = InFullName Then
                 Return True
             End If
         Next
@@ -125,9 +165,11 @@ Class ProductFile
 
     Sub GetChildren(InProduct As Product)
         For Each ChildProd As Product In InProduct.Products
+            'Add the child product to the Children list
             Children.Add(ChildProd.ReferenceProduct.Parent.FullName)
-            If ChildProd.Products.Count > 0 Then
-                GetChildren(ChildProd)
+
+            If ChildProd.Products.Count > 0 Then 'If the child product has children of its own
+                GetChildren(ChildProd) 'Recursively call GetChildren (this function) for any products that have sub-products
             End If
         Next
     End Sub
