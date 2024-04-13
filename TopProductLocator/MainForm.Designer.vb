cc95bdd8adc9c967078e30e0037a62ba3267f53b
weagan22 <weagan22@gmail.com>
2024-04-12 19:49:17 -0700

    Added status strip/status, enter key to run, sorting of results, and other small details.

--- TopProductLocator/MainForm.Designer.vb
+++ TopProductLocator/MainForm.Designer.vb
@@ -22,6 +22,7 @@ Partial Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
         Me.Box_FilePath = New System.Windows.Forms.GroupBox()
         Me.Txt_FilePath = New System.Windows.Forms.TextBox()
@@ -30,12 +31,17 @@ Partial Class MainForm
         Me.List_OutValues = New System.Windows.Forms.ListBox()
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.Btn_GetTopProd = New System.Windows.Forms.Button()
+        Me.Chk_GetParts = New System.Windows.Forms.CheckBox()
         Me.LayoutPanel_Global = New System.Windows.Forms.TableLayoutPanel()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
         Me.Box_FilePath.SuspendLayout()
         Me.LayoutPanel_PathButton.SuspendLayout()
         Me.Box_TopProds.SuspendLayout()
         Me.TableLayoutPanel1.SuspendLayout()
         Me.LayoutPanel_Global.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Box_FilePath
@@ -48,6 +54,7 @@ Partial Class MainForm
         Me.Box_FilePath.TabIndex = 0
         Me.Box_FilePath.TabStop = False
         Me.Box_FilePath.Text = "File Path"
+        Me.ToolTip1.SetToolTip(Me.Box_FilePath, "File Path")
         '
         'Txt_FilePath
         '
@@ -57,6 +64,7 @@ Partial Class MainForm
         Me.Txt_FilePath.Size = New System.Drawing.Size(472, 20)
         Me.Txt_FilePath.TabIndex = 0
         Me.Txt_FilePath.Text = "File path here..."
+        Me.ToolTip1.SetToolTip(Me.Txt_FilePath, "File path here...")
         '
         'LayoutPanel_PathButton
         '
@@ -70,8 +78,8 @@ Partial Class MainForm
         Me.LayoutPanel_PathButton.Name = "LayoutPanel_PathButton"
         Me.LayoutPanel_PathButton.RowCount = 1
         Me.LayoutPanel_PathButton.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.LayoutPanel_PathButton.Size = New System.Drawing.Size(478, 130)
-        Me.LayoutPanel_PathButton.TabIndex = 0
+        Me.LayoutPanel_PathButton.Size = New System.Drawing.Size(478, 110)
+        Me.LayoutPanel_PathButton.TabIndex = 2
         '
         'Box_TopProds
         '
@@ -79,10 +87,11 @@ Partial Class MainForm
         Me.Box_TopProds.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Box_TopProds.Location = New System.Drawing.Point(3, 3)
         Me.Box_TopProds.Name = "Box_TopProds"
-        Me.Box_TopProds.Size = New System.Drawing.Size(322, 124)
-        Me.Box_TopProds.TabIndex = 2
+        Me.Box_TopProds.Size = New System.Drawing.Size(322, 104)
+        Me.Box_TopProds.TabIndex = 1
         Me.Box_TopProds.TabStop = False
         Me.Box_TopProds.Text = "Located Top Product(s)"
+        Me.ToolTip1.SetToolTip(Me.Box_TopProds, "Located Top Product(s)")
         '
         'List_OutValues
         '
@@ -92,22 +101,25 @@ Partial Class MainForm
         Me.List_OutValues.ItemHeight = 16
         Me.List_OutValues.Location = New System.Drawing.Point(3, 16)
         Me.List_OutValues.Name = "List_OutValues"
-        Me.List_OutValues.Size = New System.Drawing.Size(316, 105)
-        Me.List_OutValues.TabIndex = 1
+        Me.List_OutValues.Size = New System.Drawing.Size(316, 85)
+        Me.List_OutValues.TabIndex = 0
+        Me.ToolTip1.SetToolTip(Me.List_OutValues, "Returned list of products that are not a child of any other products in the speci" &
+        "fied directory.")
         '
         'TableLayoutPanel1
         '
         Me.TableLayoutPanel1.ColumnCount = 1
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
         Me.TableLayoutPanel1.Controls.Add(Me.Btn_GetTopProd, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Chk_GetParts, 0, 1)
         Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.TableLayoutPanel1.Location = New System.Drawing.Point(331, 3)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
         Me.TableLayoutPanel1.RowCount = 2
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50.0!))
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(144, 124)
-        Me.TableLayoutPanel1.TabIndex = 3
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(144, 104)
+        Me.TableLayoutPanel1.TabIndex = 0
         '
         'Btn_GetTopProd
         '
@@ -116,9 +128,24 @@ Partial Class MainForm
         Me.Btn_GetTopProd.Location = New System.Drawing.Point(3, 3)
         Me.Btn_GetTopProd.Name = "Btn_GetTopProd"
         Me.Btn_GetTopProd.Size = New System.Drawing.Size(138, 44)
-        Me.Btn_GetTopProd.TabIndex = 1
+        Me.Btn_GetTopProd.TabIndex = 0
         Me.Btn_GetTopProd.Text = "Locate"
+        Me.ToolTip1.SetToolTip(Me.Btn_GetTopProd, "Find the top product(s) within the specified file path.")
         Me.Btn_GetTopProd.UseVisualStyleBackColor = True
+        '
+        'Chk_GetParts
+        '
+        Me.Chk_GetParts.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Chk_GetParts.AutoSize = True
+        Me.Chk_GetParts.Location = New System.Drawing.Point(3, 53)
+        Me.Chk_GetParts.Name = "Chk_GetParts"
+        Me.Chk_GetParts.Size = New System.Drawing.Size(138, 17)
+        Me.Chk_GetParts.TabIndex = 1
+        Me.Chk_GetParts.Text = "Get Parts"
+        Me.ToolTip1.SetToolTip(Me.Chk_GetParts, "When cheked, the function also returns parts that are not contained in a parent p" &
+        "roduct.")
+        Me.Chk_GetParts.UseVisualStyleBackColor = True
         '
         'LayoutPanel_Global
         '
@@ -126,15 +153,31 @@ Partial Class MainForm
         Me.LayoutPanel_Global.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
         Me.LayoutPanel_Global.Controls.Add(Me.LayoutPanel_PathButton, 0, 1)
         Me.LayoutPanel_Global.Controls.Add(Me.Box_FilePath, 0, 0)
+        Me.LayoutPanel_Global.Controls.Add(Me.StatusStrip1, 0, 2)
         Me.LayoutPanel_Global.Dock = System.Windows.Forms.DockStyle.Fill
         Me.LayoutPanel_Global.Location = New System.Drawing.Point(0, 0)
         Me.LayoutPanel_Global.Name = "LayoutPanel_Global"
-        Me.LayoutPanel_Global.RowCount = 2
+        Me.LayoutPanel_Global.RowCount = 3
         Me.LayoutPanel_Global.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50.0!))
         Me.LayoutPanel_Global.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
         Me.LayoutPanel_Global.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
         Me.LayoutPanel_Global.Size = New System.Drawing.Size(484, 186)
         Me.LayoutPanel_Global.TabIndex = 1
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 166)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(484, 20)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(48, 15)
+        Me.ToolStripStatusLabel1.Text = "Ready..."
         '
         'MainForm
         '
@@ -151,7 +194,11 @@ Partial Class MainForm
         Me.LayoutPanel_PathButton.ResumeLayout(False)
         Me.Box_TopProds.ResumeLayout(False)
         Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
         Me.LayoutPanel_Global.ResumeLayout(False)
+        Me.LayoutPanel_Global.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -164,4 +211,8 @@ Partial Class MainForm
     Friend WithEvents List_OutValues As ListBox
     Friend WithEvents Box_TopProds As GroupBox
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Chk_GetParts As CheckBox
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
 End Class
--- TopProductLocator/MainForm.vb
+++ TopProductLocator/MainForm.vb
@@ -1,5 +1,7 @@
 ﻿Imports INFITF
 Imports ProductStructureTypeLib
+Imports System.Drawing.Imaging
+Imports System.IO
 
 Public Class MainForm
 
@@ -9,6 +11,10 @@ Public Class MainForm
         If System.IO.Directory.Exists(Txt_FilePath.Text) Then
             Btn_GetTopProd.Enabled = True
             Txt_FilePath.BackColor = Color.White
+
+        ElseIf Txt_FilePath.Text = "File path here..." Then
+            Txt_FilePath.BackColor = Color.White
+
         Else
             Btn_GetTopProd.Enabled = False
             Txt_FilePath.BackColor = Color.Salmon
@@ -24,6 +30,9 @@ Public Class MainForm
             If GetCATIA() Then 'Make sure CATIA is open and available
                 'Get the file path from the form
                 Dim FilePath As String = Txt_FilePath.Text
+                If Not Directory.Exists(FilePath) Then
+                    Throw New Exception("Specified directory does not exist.")
+                End If
 
                 'Load the list of products (get their children)
                 Dim ProductList = GetProducts(FilePath)
@@ -31,15 +40,21 @@ Public Class MainForm
                 'Check each product to see if it is a child of any of the others
                 Call CheckChildren(ProductList)
 
+                'Sort the product list so products are first
+                ProductList.Sort(New ProductFileComparer())
+
                 'Output back to the form
                 OutputTopProducts(ProductList)
 
             End If
 
+            ToolStripStatusLabel1.Text = "Ready..."
+
         Catch ex As Exception 'Handle catastrophic error
             MsgBox("Error occured while locating top product." & vbNewLine & vbNewLine & ex.Message, MessageBoxIcon.Warning, "Error")
 
             Try 'Finally, make sure file alerts get turned back on in CATIA
+                ToolStripStatusLabel1.Text = "Ready..."
                 MainForm.CATIA.DisplayFileAlerts = True
             Catch ex2 As Exception
 
@@ -51,24 +66,42 @@ Public Class MainForm
         Dim FileList() As String = System.IO.Directory.GetFiles(FilePath)
         Dim RetProductList = New List(Of ProductFile)
 
-        'Get all of the products and their children in the directory
-        For i As Integer = 0 To UBound(FileList)
-            Dim StrFile As String = FileList(i)
+        'Get the number of files that are going to be checked to provide status
+        Dim StatusFileCount As Integer = StatusCount(FileList)
 
+        'Get all of the products and their children in the directory
+        Dim StatusCnt As Integer = 0
+        For i As Integer = 0 To UBound(FileList)
+
+            Dim StrFile As String = FileList(i)
             Dim ExtType As String = System.IO.Path.GetExtension(StrFile)
-            If ExtType = ".CATProduct" Or ExtType = ".CATPart" Then
-                'This conditional limits the search to only .CATProduct files.
-                'If you would also like to return other file types
-                'in the directory that don't have a parent then you
-                'can remove this conditional. You could also extend it to other
-                'specific types like '... OR ExtType = ".CATPart" Then'
-                'for example.
+
+            If ExtType = ".CATProduct" Or (Chk_GetParts.Checked And ExtType = ".CATPart") Then 'This conditional limits the search to only .CATProduct files unless Get Parts is checked.
+
+                If ExtType = ".CATProduct" Then 'Only status products since they are what take time to load in CATIA
+                    StatusCnt += 1
+                    ToolStripStatusLabel1.Text = $"Running Locate | Files: {StatusCnt}/{StatusFileCount} | Current File: {Path.GetFileName(StrFile)}"
+                End If
 
                 RetProductList.Add(New ProductFile(StrFile))
             End If
         Next
 
         Return RetProductList
+    End Function
+
+    Function StatusCount(FileList() As String) As Integer
+        'Get the number of files that are going to be checked to provide status
+        Dim ReturnFileCount As Integer
+        For Each StrFile As String In FileList
+            Dim ExtType As String = Path.GetExtension(StrFile)
+
+            If ExtType = ".CATProduct" Then 'Only status products since they are what take time to load in CATIA
+                ReturnFileCount += 1
+            End If
+        Next
+
+        Return ReturnFileCount
     End Function
 
     Sub CheckChildren(ProductList As List(Of ProductFile))
@@ -85,6 +118,7 @@ Public Class MainForm
     End Sub
 
     Sub OutputTopProducts(ProductList As List(Of ProductFile))
+
         'For each of the products, output to the listbox if the IsChild handle has not been set
         For Each Prod In ProductList
             If Not Prod.Child Then
@@ -94,10 +128,9 @@ Public Class MainForm
 
         'Handle no products in the directory
         If List_OutValues.Items.Count = 0 Then
-            List_OutValues.Items.Add("Error: No CATProducts found in the directory")
+            List_OutValues.Items.Add("No products located in the specified directory")
         End If
     End Sub
-
 
     Function GetCATIA() As Boolean
         Dim countTest As Integer = 0
@@ -128,6 +161,52 @@ Public Class MainForm
 
         Return True
     End Function
+
+#Region "Enter key to run on all features"
+
+    Private Sub Txt_FilePath_KeyDown(sender As Object, e As KeyEventArgs) Handles Txt_FilePath.KeyDown
+        Call EnterKeyRun(sender, e)
+    End Sub
+    Private Sub Chk_GetParts_KeyDown(sender As Object, e As KeyEventArgs) Handles Chk_GetParts.KeyDown
+        Call EnterKeyRun(sender, e)
+    End Sub
+    Private Sub List_OutValues_KeyDown(sender As Object, e As KeyEventArgs) Handles List_OutValues.KeyDown
+        Call EnterKeyRun(sender, e)
+    End Sub
+    Private Sub Btn_GetTopProd_KeyDown(sender As Object, e As KeyEventArgs) Handles Btn_GetTopProd.KeyDown
+        Call EnterKeyRun(sender, e)
+    End Sub
+    Sub EnterKeyRun(sender As Object, e As KeyEventArgs)
+        If e.KeyValue = Keys.Enter Then
+            If Btn_GetTopProd.Enabled Then
+                Call Btn_GetTopProd_Click(sender, e)
+            End If
+        End If
+    End Sub
+#End Region
+
+    Class ProductFileComparer
+        Implements IComparer(Of ProductFile)
+
+        Private Declare Unicode Function StrCmpLogicalW Lib "shlwapi" (ByVal s1 As String, ByVal s2 As String) As Integer
+
+        Public Function Compare(F1 As ProductFile, F2 As ProductFile) As Integer Implements IComparer(Of ProductFile).Compare
+
+            'Sort products before parts
+            If F1.IsProduct And Not F2.IsProduct Then
+                Return -1
+            End If
+
+            If Not F1.IsProduct And F2.IsProduct Then
+                Return 1
+            End If
+
+            'If the same file type then just sort by name, ignore extensions for sorting purposes
+            Return StrCmpLogicalW(Path.GetFileNameWithoutExtension(F1.Name), Path.GetFileNameWithoutExtension(F2.Name))
+
+        End Function
+
+    End Class
 End Class
 
 Public Class ProductFile
@@ -135,21 +214,27 @@ Public Class ProductFile
     Public Name As String 'The name of the file
     Public Children = New List(Of String) 'List of all child products
     Public Child As Boolean = False 'Boolean for if this product is a child of others
+    Public IsProduct As Boolean = False
 
     Sub New(FilePath As String)
         FullName = FilePath
-        Name = System.IO.Path.GetFileName(FilePath)
+        Name = Path.GetFileName(FilePath)
 
-        'Open the file for reading in CATIA
-        MainForm.CATIA.DisplayFileAlerts = False
-        Dim Doc As Document = MainForm.CATIA.Application.Documents.Read(FilePath)
-        MainForm.CATIA.DisplayFileAlerts = True
+        If Path.GetExtension(FullName) = ".CATProduct" Then IsProduct = True
 
-        'Find all of its child products
-        GetChildren(Doc.Product)
+        If IsProduct Then 'Get the products children, skips this for parts
+            'Open the file for reading in CATIA
+            MainForm.CATIA.DisplayFileAlerts = False
+            Dim Doc As Document = MainForm.CATIA.Application.Documents.Read(FilePath)
+            MainForm.CATIA.DisplayFileAlerts = True
 
-        'Close the document for reading
-        Doc.Close()
+            'Find all of its child products
+            GetChildren(Doc.Product)
+
+            'Close the document for reading
+            Doc.Close()
+        End If
+
     End Sub
 
     Function IsChild(InFullName As String) As Boolean
@@ -173,4 +258,6 @@ Public Class ProductFile
             End If
         Next
     End Sub
+
+
 End Class
